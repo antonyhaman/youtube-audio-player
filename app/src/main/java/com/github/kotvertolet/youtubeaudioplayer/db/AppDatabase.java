feacb27bb85657ab8db0bf098fa088e4a0d8943a
kotvertolet <bytesurfer5005@gmail.com>
2019-07-08 07:05:35 +0300

    apk updated

--- app/src/main/java/com/github/kotvertolet/youtubeaudioplayer/db/AppDatabase.java
+++ app/src/main/java/com/github/kotvertolet/youtubeaudioplayer/db/AppDatabase.java
@@ -22,7 +22,7 @@ public abstract class AppDatabase extends RoomDatabase {
 
     private static AppDatabase instance;
 
-    public static synchronized AppDatabase getInstance(Context context) {
+    public static AppDatabase getInstance(Context context) {
         if (instance == null) {
             instance = Room.databaseBuilder(context.getApplicationContext(),
                     AppDatabase.class, "YtData.db")
